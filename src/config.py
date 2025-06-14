db1387ae9fcbbbc5cdf72d49f9e7af48d007c937
zhuwj <byzhubaiyuan@gmail.com>
2025-06-14 18:13:42 +0800

    Add ruff, black, ci

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     MODEL_PATH: str = "artifacts/infer-net.keras"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
